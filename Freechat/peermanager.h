609febbd08a785a55b031c5678169b4eda36ca45
bogdyname <bogdyname@gmail.com>
2019-03-21 06:42:13 -0400

    update peermanager.h&userclient.h -- added fun for connection and check connection -- connected PreeManager class with UserClient&Connecion classes -- refactoring userclient.h (all file) -- refactoring peermanager.h (all file)

--- Freechat/peermanager.h
+++ Freechat/peermanager.h
@@ -1,11 +1,50 @@
+/*
+***Copyright (C) 2019 Freechat Inc
+***Contact: bogdyname@gmail.com
+*/
+
 #ifndef PEERMANAGER_H
 #define PEERMANAGER_H
 
+#include <QList>
+#include <QTimer>
+#include <QObject>
+#include <QByteArray>
+#include <QUdpSocket>
 
-class peermanager
+class UserClient;
+class Connection;
+
+class PeerManager : public QObject
 {
+    Q_OBJECT
+
 public:
-    peermanager();
+    PeerManager(UserClient *userclient);
+
+    QString userName() const;
+    void startBroadcasting();
+    void setServerPort(int port);
+    bool isWLANHostAddress(const QHostAddress &address);
+
+signals:
+    void newConnection(Connection *connection);
+
+private slots:
+    void sendBroadcastDatagram();
+    void readBroadcastDatagram();
+
+private:
+    void updateAddresses();
+
+    UserClient *userclient;
+
+    int serverPort;
+    QString username;
+    QTimer broadcastTimer;
+    QUdpSocket broadcastSocket;
+    QList<QHostAddress> ipAddresses;
+    QList<QHostAddress> broadcastAddresses;
 };
 
 #endif // PEERMANAGER_H
