84b4db8e68879d07a95b8f6f8cb0521cafcc0d49
bogdyname <bogdyname@gmail.com>
2019-06-12 05:22:11 -0400

    added new classes -- added new class for Host code with funs of network code and TCPsocket -- added new class for Client code with funs of network code and TCPsocket

--- Freechat/connectionf2f.h
+++ Freechat/connectionf2f.h
@@ -3,9 +3,6 @@
 ***Contact: bogdyname@gmail.com
 */
 
-#ifndef CONNECTIONF2F_H
-#define CONNECTIONF2F_H
-
 #include <QNetworkInterface>
 #include <QAbstractSocket>
 #include <QNetworkSession>
@@ -20,24 +17,24 @@
 using namespace QSsl;
 using namespace QPasswordDigestor;
 
+#ifndef CONNECTIONF2F_H
+#define CONNECTIONF2F_H
+
 class ConnectionF2F : public QTcpSocket
 {
     Q_OBJECT
 
     ConnectionF2F(QObject *parent = nullptr);
+    ~ConnectionF2F();
 
 private slots:
     void DigitalSignature();
     void ConnectingToPeer();
     void SocketConnected();
     void SocketDisconnected();
-    void ReadyForReadOfData();
+    void ReadyReadOfData();
 
-public:
-    void SockedErroe();
-    void MakeSocket();
-
-protected:
+private:
     quint16 GetPeerPort();
     QHostAddress GetPeerAddress();
 
@@ -47,3 +44,52 @@ private:
 };
 
 #endif // CONNECTIONF2F_H
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+class Client : public QTcpSocket
+{
+    Q_OBJECT
+
+public:
+    Client(QObject *parent = nullptr);
+    ~Client();
+
+    void GetSocketFromClient();
+
+private:
+    void MakeSocketClient();
+    void SockedErroe();
+
+public:
+    quint16 GetPeerPort();
+    QHostAddress GetPeerAddress();
+
+private:
+    quint16 PeerPort();
+    QHostAddress PeerAddress();
+};
+
+#endif // CLIENT_H
+
+#ifndef HOST_H
+#define HOST_H
+
+class Host : public QTcpServer
+{
+    Q_OBJECT
+
+public:
+    Host(QObject *parent = nullptr);
+    ~Host();
+
+    void GetSocketFromHost();
+
+private:
+    void MakeSocketHost();
+    void SockedErroe();
+    void ListenPort();
+};
+
+#endif // HOST_H
