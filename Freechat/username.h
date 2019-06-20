3cbbb9cf31da865ac8b8533654ee0a21ca87f02e
bogdyname <43905628+bogdyname@users.noreply.github.com>
2019-06-20 23:46:37 +0300

    refactoring

--- Freechat/username.h
+++ Freechat/username.h
@@ -3,13 +3,23 @@
 ***Contact: bogdyname@gmail.com
 */
 
-#ifndef USERNAME_H
-#define USERNAME_H
-
+#include <QtNetwork/QNetworkInterface>
+#include <QtCore/QCoreApplication>
+#include <QAbstractSocket>
+#include <QHostAddress>
+#include <QJsonObject>
 #include <QTextStream>
 #include <QDataStream>
+#include <QByteArray>
+#include <QIODevice>
+#include <QHostInfo>
+#include <QSaveFile>
+#include <QDateTime>
 #include <QString>
 #include <QFile>
+
+#ifndef USERNAME_H
+#define USERNAME_H
 
 class Freechat;
 class Connection;
@@ -23,8 +33,31 @@ public:
     Username(QObject *parent = nullptr);
 
 public slots:
-    void ReadingIpAddress();
-    void ReadingMACAddress();
+    inline void ReadingIpAddress(QFile &fileWithIP);
+    inline void ReadingMACAddress(QFile &fileWithMac);
+    void TranslationName(QFile &fileWithMAC, QString &translator);
 };
 
 #endif // USERNAME_H
+
+#ifndef USERNAMETABLE_H
+#define USERNAMETABLE_H
+
+class Username;
+
+class Usernametable : public QFile
+{
+    Q_OBJECT
+
+public:
+    ~Usernametable();
+    Usernametable(QObject *parent = nullptr);
+    Usernametable(Usernametable &&MoveNameSource, QFile &);
+
+private:
+    void GetIpAddressFromWAN(QString &textWithIPAddres);
+    inline void GetMacAddress(QString &textWithMacAddresOfUser);
+};
+
+
+#endif // USERNAMETABLE_H
