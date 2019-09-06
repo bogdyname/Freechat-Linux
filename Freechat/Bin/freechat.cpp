c723ca8bc862f71bc8df83b8636f5a7a98e63b85
bogdyname <43905628+bogdyname@users.noreply.github.com>
2019-09-06 21:53:45 +0300

    Update freechat.cpp

--- Freechat/Bin/freechat.cpp
+++ Freechat/Bin/freechat.cpp
@@ -24,11 +24,66 @@ void Freechat::on_showNetworkInfo_clicked(bool checked)
     {
     case true:
         QMessageBox::information(this, tr("Network Info"),
-                                 tr("Your IP address: "), yourIp);
+                                 tr("Your LAN IP address: "), yourIp);
         break;
     case false:
         break;
     }
 
     return;
+}
+
+void Freechat::on_connectionToPeer_clicked(bool checked)
+{
+    bool reply = false;
+
+    switch(checked)
+    {
+    case true:
+    {
+        QMessageBox::information(this, tr("Connection"),
+                                 tr("Connecting to peer..."));
+
+        AskForConnectingToPortPeer(); //ask for connection
+
+        if(ReplyFromPortPeer(reply) == true) // reply
+        {
+
+        }
+        else
+        {
+            QMessageBox::information(this, tr("Disconnected"),
+                                     tr("Peer does not want to connect with you"));
+        }
+    }
+        break;
+    case false:
+        break;
+    }
+
+    return;
+}
+
+void Freechat::AskForConnectingToPortPeer()
+{
+
+    return;
+}
+
+void Freechat::AskForDisconnectingFromPortPeer()
+{
+
+    return;
+}
+
+bool Freechat::ReplyFromPortPeer(bool &reply)
+{
+    if(reply == true)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
