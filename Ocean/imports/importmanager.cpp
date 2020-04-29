d24a6136b26bd77f199e8f680aa7c8e8e630cc78
Bogdyname <bogdyname@gmail.com>
2020-04-29 19:45:52 +0300

    opne files
    -- save one file into music folder there is app folder
    -- create music folder

--- Ocean/imports/importmanager.cpp
+++ Ocean/imports/importmanager.cpp
@@ -10,7 +10,8 @@ ImportManager::ImportManager()
 {
     try
     {
-        ImportManager::mp3 = new QFile();
+        ImportManager::musicDir = new QDir();
+        ImportManager::mp3File = new QFile();
         ImportManager::importerWindow = new QFileDialog();
     }
     catch (std::bad_alloc &exp)
@@ -28,22 +29,102 @@ ImportManager::ImportManager()
         abort();
     }
 
+    //current path of app
+    ImportManager::musicDir->QDir::setCurrent(QCoreApplication::applicationDirPath());
+
+    //Check folder of music
+    if(ImportManager::musicDir->QDir::mkdir("music"))
+        qDebug() << "good";
+    else
+        qDebug() << "not good";
+
+    //Finder for looking for music
     ImportManager::importerWindow->QFileDialog::setDirectory(QDir::rootPath());
 
     //SIGNALS WITH SLOTS
     //looking for file and save if into own folder
-    QObject::connect(ImportManager::importerWindow, SIGNAL(fileSelected(QString)), this, SLOT(SaveFileIntoMusicFolder(QString)));
+    QObject::connect(ImportManager::importerWindow, SIGNAL(fileSelected(const QString)), this, SLOT(SaveFileIntoMusicFolder(const QString)));
+    QObject::connect(ImportManager::importerWindow, SIGNAL(filesSelected(QStringList)), this, SLOT(SaveFilesIntoMusicFolder(QStringList)));
+
+
+    //TTS
+    QString path = importerWindow->getOpenFileName(0, "TTS", "", "*.jpg");
+
+    if(path.isEmpty())
+        return;
+    else
+        SaveFileIntoMusicFolder(path);
+    //TTS
 }
 
 ImportManager::~ImportManager()
 {
-    delete ImportManager::mp3;
+    delete ImportManager::musicDir;
+    delete ImportManager::mp3File;
     delete ImportManager::importerWindow;
 }
 
+//Slots for save files
 void ImportManager::SaveFileIntoMusicFolder(const QString &pathOfmp3)
 {
-    ImportManager::mp3->setFileName(pathOfmp3);
+    //Check path of file
+    if(!pathOfmp3.isEmpty())
+        ImportManager::mp3File->QFile::setFileName(pathOfmp3);
+    else
+        return;
+
+    //copy file into music folder there is folder of app and delete it
+    if(ImportManager::mp3File->QFile::open(QIODevice::ReadOnly))
+    {
+        ImportManager::CheckDir();
+
+        //Need to rework it for save by only last name of the file (like this -> "I see fire.mp3")
+        //For this case need to sort out the string of current path of file
+        ImportManager::mp3File->QFile::copy(pathOfmp3, ImportManager::musicDir->QDir::currentPath() + "/music/" + "photo.jpg");
+        ImportManager::mp3File->QFile::remove();
+
+        #ifndef Q_DEBUG
+        qDebug() << "File has been removed: " + pathOfmp3;
+        #endif
+    }
+    else
+    {
+        #ifndef Q_DEBUG
+        qCritical() << "Error open or read file!";
+        #endif
+
+        return;
+    }
+
+    ImportManager::mp3File->close();
 
     return;
+}
+
+void ImportManager::SaveFilesIntoMusicFolder(const QStringList &pathsOfmp3)
+{
+
+    return;
+}
+
+bool ImportManager::CheckDir()
+{
+    if(QDir("music").QDir::exists() == false)
+    {
+        ImportManager::musicDir->QDir::mkdir("music");
+
+        #ifndef Q_DEBUG
+        qDebug() << "Folder 'music' created";
+        #endif
+
+        return false;
+    }
+    else
+    {
+        #ifndef Q_DEBUG
+        qCritical() << "Folder 'music' already exists!";
+        #endif
+
+        return true;
+    }
 }
--- Ocean/imports/importmanager.h
+++ Ocean/imports/importmanager.h
@@ -12,6 +12,7 @@
 #include <QFileDialog>
 
 #include <QDebug>
+#include <QCoreApplication>
 
 class ImportManager : public QFile
 {
@@ -21,16 +22,19 @@ public:
     ImportManager();
     ~ImportManager();
 
-public slots:
+private slots:
     //SavingFiles
     //for file dialog (looking for file in finder)
-    void SaveFileIntoMusicFolder(const QString &pathOfmp3);
+    void SaveFileIntoMusicFolder(const QString &pathOfmp3); //NOT DONE
+    void SaveFilesIntoMusicFolder(const QStringList &pathsOfmp3); //NOT DONE
 
 private:
+    bool CheckDir();
 
     //Objects of Qt
 private:
-    QFile *mp3 = nullptr;
+    QDir *musicDir = nullptr;
+    QFile *mp3File = nullptr;
     QFileDialog *importerWindow = nullptr;
 };
 
